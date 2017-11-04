11c86c7e8f1888173e1afd3ba928941ba07fd342
the-dentist <billy.juergens@gmail.com>
2017-11-03 21:02:43 -0700

    xp system 1st draft

---
+++ Meteorfire-Prototype/Assets/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class determines the combat abilities of the enemies 
+// The enemies "level up" similarly to how the player does
+public class EnemyController : MonoBehaviour {
+	// The "level" of the enemy controller is similar to a wave number
+	// the higher it is the harder the waves get
+	protected int level;
+
+	public void start() {
+		level = 1;
+	}
+
+	public void levelUp() {
+		level += 1;
+	}
+
+	public int getLevel() { return level; }
+
+	// TODO some kind of template for enemy level 1/2/3 or some algorithm to determine their stats
+}
---
+++ Meteorfire-Prototype/Assets/EnemyXPManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+// this class is intended to be attached to the enemy controller object
+// this class manages the progress of the enemy controller
+public class EnemyXPManager : XPManager {
+	protected EnemyController ec;
+
+	public void start() {
+		ec = gameObject.GetComponent<EnemyController> ();
+		current_xp = 0;
+		required_xp = determineRequiredXP ();
+	}
+
+	protected override void levelUp() {
+		ec.levelUp ();
+	}
+		
+	protected override void determineXPGain(Unit source, Unit victim) {
+		if (victim.tag == "Enemy") {
+			gainXP(1);
+		} else if (source.tag == "Boss") {
+			gainXP(10);
+		}
+	}
+
+	protected override int determineRequiredXP() {
+		return ec.getLevel () * 10;
+	}
+}
